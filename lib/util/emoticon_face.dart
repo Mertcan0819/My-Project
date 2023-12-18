4c73cac400d38e56b8692d139fdbc1bd2286d03b
Mert Can <123500356+Mertcan0819@users.noreply.github.com>
2023-12-18 14:58:38 +0300

    Update emoticon_face.dart

--- lib/util/emoticon_face.dart
+++ lib/util/emoticon_face.dart
@@ -7,13 +7,13 @@ final String emoticonface;
 
   @override
   Widget build(BuildContext context) {
-    return Container(                                                 // önce icon açıp sonra container ile sarmaladık ve  decoration yaptık 
+    return Container(                                           
                            decoration: BoxDecoration(
                              color: Colors.blue[600],
                              borderRadius: BorderRadius.circular(12) 
                             ),
-                             padding: const EdgeInsets.all(12),                           // dikkat et padding i container içinde verdik. yani paddik ile sarmalamadık. bu sayede container in içine paddingn vermiş olduk.
-                             child: Center(child: Text(emoticonface, style: const TextStyle(fontSize: 28),)),   // emoticonface burada gülücük emojisi vardı final ile yukarıda tanımladık
+                             padding: const EdgeInsets.all(12),                     
+                             child: Center(child: Text(emoticonface, style: const TextStyle(fontSize: 28),)),
                          );
   }
 }
